017982a6784c42f7012c4d2b184cb31c2201a86e
knz13 <82895172+knz13@users.noreply.github.com>
2022-01-24 05:27:33 -0300

    adding argparse

--- one_file.py
+++ one_file.py
@@ -1,12 +1,21 @@
 import os
 from PyQt5 import *
 from typing import List
+import argparse
+
+parser = argparse.ArgumentParser(description="Useful script to create one python file from multiple ones in the same folder")
+parser.add_argument("main_filename",metavar="F",type=str,nargs="+")
+parser.add_argument("destination_filename",metavar="D",type=str,nargs="+")
+
+args = parser.parse_args()
+
+
 
 def get_contents_from_sub_file(filename: str,current_files : List[str],directory: str):
     
     final_file = ""
     
-    with open(directory + "/" + filename + ".py", "r") as file:
+    with open(directory + "/" + filename + ".py", "r",errors="replace") as file:
         file_contents = file.readlines()
     
     for line in file_contents:
@@ -35,10 +44,10 @@ def create_one_file(main_file: str,final_file_name: str,directory : str = os.get
     file_names_in_current_folder = [i[:len(i)-3] for i in os.listdir(directory) if i.endswith(".py")]
     final_file = ""
     
-    final_file += get_contents_from_sub_file(main_file[:len(main_file)-3],file_names_in_current_folder,directory)
+    final_file += get_contents_from_sub_file(main_file,file_names_in_current_folder,directory)
     
-    with open(directory + "/" + final_file_name + ".py","w") as file:
+    with open(directory + "/" + final_file_name + ".py","w",errors="replace") as file:
         file.write(final_file)
         
 
-
+create_one_file(args.main_filename[0],args.destination_filename[0])
